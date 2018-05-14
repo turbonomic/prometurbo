e7020d4b14dacaeba8f0ab6a7aba68c177b846dc
songbinliu <songbin.liu@turbonomic.com>
2018-05-14 17:37:38 -0400

    add comments

--- appmetric/scripts/docker/run.sh
+++ appmetric/scripts/docker/run.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+## start the docker container in local for testing
 
 ##NOTE: should use the ip address, instead of 'localhost' or '127.0.0.1'
 url=10.10.2.3:9090
 
-docker run -d -p 18081:8081 beekman9527/appmetric:v2 --promUrl=$url --v=3 --logtostderr
+docker run -d -p 18081:8081 vmturbo/appmetric:v2 --promUrl=$url --v=3 --logtostderr
 sleep 1
 docker ps

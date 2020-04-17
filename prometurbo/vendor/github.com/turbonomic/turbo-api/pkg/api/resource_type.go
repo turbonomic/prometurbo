3b1fee2f313987880bba287ce1ae01ac5cf7fa6c
Meng Ding <dingmeng@gmail.com>
2020-04-17 12:20:12 -0400

    Delete prometurbo folder

--- prometurbo/vendor/github.com/turbonomic/turbo-api/pkg/api/resource_type.go
+++
@@ -1,9 +0,0 @@
-package api
-
-type ResourceType string
-
-const (
-	Resource_Type_Reservation     ResourceType = "reservations"
-	Resource_Type_Target          ResourceType = "targets"
-	Resource_Type_External_Target ResourceType = "externaltargets"
-)

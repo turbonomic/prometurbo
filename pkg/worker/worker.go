8478e5e348df78451e9d733bc2660b53d9509045
Travis CI User <travis@example.org>
2025-02-28 03:02:42 -0500

    Preparing for https://github.com/turbonomic/prometurbo

---
+++ pkg/worker/worker.go
@@ -0,0 +1,23 @@
+package worker
+
+import "github.ibm.com/turbonomic/turbo-go-sdk/pkg/dataingestionframework/data"
+
+type ITask interface {
+	Run() []*data.DIFEntity
+}
+
+type worker struct {
+	id       string
+	taskChan chan ITask
+}
+
+func newWorker(id string) *worker {
+	return &worker{
+		id:       id,
+		taskChan: make(chan ITask),
+	}
+}
+
+func (w *worker) execute(task ITask) []*data.DIFEntity {
+	return task.Run()
+}
